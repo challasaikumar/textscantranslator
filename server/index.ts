79ae06e8a0c015dd90c72a0dd2d2be9a76753b52
Challasai916 <challasai916@gmail.com>
2025-05-14 16:49:35 +0530

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -59,12 +59,17 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+const port = 5000;
+const host = "localhost"; // or "0.0.0.0"
+
+server.listen({ port, host }, () => {
+  const address =
+    host === "localhost"
+      ? `http://localhost:${port} OR http://<your-local-IP>:${port}`
+      : `http://${host}:${port}`;
+
+  console.log(`✅ Server is running at: ${address}`);
+});
+
+
 })();
